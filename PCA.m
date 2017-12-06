c3f7646ce6c0a91c29becda22bb3ba89fec2fe87
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-06 20:04:20 -0200

    Update PCA.m

--- PCA.m
+++ PCA.m
@@ -1,18 +1,17 @@
 function [autovalor, autovetor, nova_matriz, EQM] = PCA(matriz,m)
 
     [L,N] = size(matriz); % dimensoes da matriz de dados (L caracteristicas x N padroes)
-    matriz = matriz - repmat(mean(matriz,2),1,N); % subtraindo todos os valores pelas medias das caracteristicas
-    matriz_correlacao = matriz*matriz'; % gerando a matriz de correlacao
+    
+    matriz_correlacao = (matriz*matriz')* (1/N); % gerando a matriz de correlacao
 
     [autovetor,autovalor] = eig(matriz_correlacao); % autovalor e autovetor
 
-    autovalor1(:,1) = autovalor(autovalor~=0);  % apenas diagonal
-    autovalor1(:,2) = 1:length(autovalor1(:,1)); % atribuindo uma ordem de 1 até o tamanho do vetor de autovalores
-    autovalor1 = flipud(sortrows(autovalor1,1)); % ordenando 
+    autovalor = sum(autovalor);  % apenas diagonal
+    
+    carac_desejadas = autovalor(end:end-m:-1);
+    
+    nova_matriz = matriz' * autovetor(:,end:end-m+1:-1); % projetando a matriz no novo espaco
 
-    carac_desejadas = autovalor1(1:m,2);
-    nova_matriz = matriz' * autovetor(:,carac_desejadas); % gerando a matriz apenas com as caracteristicas desejadas
-
-    EQM = (sum(autovalor1(m+1:end,1)))/(sum(autovalor1(:,1)));
+    EQM = (sum(autovalor1(carac_desejadas))/(sum(autovalor);
 
 end
