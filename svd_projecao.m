f0e7359bb55e5a386e7328f9ba5c61c6cc0c753f
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 09:18:29 -0200

    Update svd_projecao.m

--- svd_projecao.m
+++ svd_projecao.m
@@ -1,5 +1,14 @@
 function [U,S,V,novo] = svd_projecao(matriz,m)
 
+
+% Autor: André Luiz Costa de Arruda
+% Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
+% Se quiser, manda uma mensagem no face, whats ou fumaça
+% Pode me seguir no instagram.
+% Sou novo no github, não me julgue.
+% Grato :D
+
+
     %INPUTS:
     %   - matriz = matriz LxN
     %   - m = numero de caracteristicas desejadas
@@ -10,7 +19,7 @@ function [U,S,V,novo] = svd_projecao(matriz,m)
     %   - V = autovetores normalizados
     %	- novo = matriz projetada em um novo espaco
     
-    [U,S,V] = svd(matriz);
-		novo = (matriz' * U(:,end:-1:end-m+1))';
+ 	[U,S,V] = svd(matriz);
+   	novo = (matriz' * U(:,end:-1:end-m+1))';
     
 end    
