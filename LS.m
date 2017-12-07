b5508da719b13ecfc3f9f615a9c553ba7cd99394
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 08:53:41 -0200

    Create LS.m

---
+++ LS.m
@@ -0,0 +1,35 @@
+function [w] = LS(m1,m2,a)
+
+    % FUNCAO PARA CALCULAR O CLASSIFICADOR LS
+
+    %INPUTS: 
+    %  - m1: matriz com os dados da classe 1
+    %  - m2: matriz com os dados da classe 2
+    %  - a: parametro de regularizacao da inversao matricial
+
+    %OUTPUTS:
+    %  - w: pesos do classificador linear 
+
+    L = size(m1,1); % numero de caracteristicas dos dados da classe 1
+    N1 = size(m1,2); % numero de padroes dos dados da classe 1
+    N2 = size(m2,2); % numero de padroes dos dados da classe 2
+
+    if(L ~= size(m2,1)) % se o numero de caracteristicas das classes for diferente
+      error('Numero de caracteristicas diferentes');
+    else
+      m1(end+1,:) = 1; % colocando 1 no vetor
+      m2(end+1,:) = 1; % colocando 1 no vetor
+      y = [m1,m2]; % y é a juncao das classes
+      classes = repmat([ones(1,N1),-1*ones(1,N2)],L+1,1); % identifica quem eh positivo e quem eh negativo (classe 1 ou classe 2)
+
+      p1 = zeros(L+1,L+1); % Iniciando p1 = 0.              p1 = inv(sum(y*y' + aI))
+      p2 = sum(classes.*y,2); % p2 = vetor y * (1 ou -1 que classifica cada classe)
+
+      for i = 1: N1+N2 % calculando o somatorio de p1
+          p1 = p1 + (y(:,i) * y(:,i)'); % pegando cada coluna de y e multiplicando por seu transposto
+      end
+
+      w = inv(p1 + a*eye(L+1,L+1)) * p2;  % calculando w
+    end
+end
+  
