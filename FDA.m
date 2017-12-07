860a706c6c60a722374ce809e17f63762bf06e9d
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 15:03:54 -0200

    Update FDA.m

--- FDA.m
+++ FDA.m
@@ -20,6 +20,6 @@ function vetor = FDA(Sw,Sb)
     [autovetor,autovalor] = eig(d); % diagonalizando matriz
     autovalor = round(autovalor,5); % arredonando os valores para nao pegar quem é muito proximo de zero
     pos = (sum(autovalor)) ~=0; % encontrando quem é diferente de zero
-    vetor = fliplr(autovetor(:,pos)); % ordenando os autovetores selecionados (que possuem autovalor diferente de zero)
+    vetor = autovetor(:,pos); % autovetores selecionados (que possuem autovalor diferente de zero)
     
 end
