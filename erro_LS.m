3ec7fba0d4fc06d747c2c92f5a1fb8ff61943a8b
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 09:14:05 -0200

    Update erro_LS.m

--- erro_LS.m
+++ erro_LS.m
@@ -1,5 +1,7 @@
 function erro = erro_LS(dados,classes,w)
 
+% Autor: André Luiz Costa de Arruda
+
 % INPUTS:
 %   - dados: matriz com todos as classes juntas (por favor, junte as classes em uma matriz)
 %   - classes: dados previamente classificados para verificarmos o erro de LS
