114a604e7b743b14c0411b976905b8a9b2e3a70f
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 09:21:08 -0200

    Update SVD.m

--- SVD.m
+++ SVD.m
@@ -1,5 +1,12 @@
 function [U,S,V] = SVD(matriz)
 
+% Autor: André Luiz Costa de Arruda
+% Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
+% Se quiser, manda uma mensagem no face, whats ou fumaça
+% Pode me seguir no instagram.
+% Sou novo no github, não me julgue.
+% Grato :D
+
     %INPUTS:
     %   - matriz = matriz LxN
     %   - m = numero de caracteristicas desejadas
