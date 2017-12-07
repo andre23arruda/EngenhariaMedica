774c774fb8b83267e5655ce8d1a9f15cf60cbae4
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-07 09:13:22 -0200

    Update erro_LS.m

--- erro_LS.m
+++ erro_LS.m
@@ -1,11 +1,12 @@
 function erro = erro_LS(dados,classes,w)
 
 % INPUTS:
-
-
+%   - dados: matriz com todos as classes juntas (por favor, junte as classes em uma matriz)
+%   - classes: dados previamente classificados para verificarmos o erro de LS
+%   - w: vetor w que classifca os dados
 
     y = [dados;ones(1,length(dados))]; % colocando o um depois da ultima caracteristica em todos os padrões
-    pk = sign(w'*y);
-    erro = (sum(pk ~= classes))/length(classes);
+    pk = sign(w'*y); % verificando o sinal de cada padrão classificado com LS. 1 = classe1, -1 = classe2
+    erro = (sum(pk ~= classes))/length(classes); % verificando o numero de erros
     
 end
