a3625cb83770e7c2287292af6ab77aa56482dd5f
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-08 08:12:35 -0200

    Update FDA.m

--- FDA.m
+++ FDA.m
@@ -18,7 +18,7 @@ function vetor = FDA(Sw,Sb)
 
     d = inv(Sw) * (Sb); 
     [autovetor,autovalor] = eig(d); % diagonalizando matriz
-    autovalor = round(autovalor,5); % arredonando os valores para nao pegar quem é muito proximo de zero
+    autovalor(autovalor>0.000001) = 0; % arredonando os valores para nao pegar quem é muito proximo de zero
     pos = (sum(autovalor)) ~=0; % encontrando quem é diferente de zero
     vetor = autovetor(:,pos); % autovetores selecionados (que possuem autovalor diferente de zero)
     
