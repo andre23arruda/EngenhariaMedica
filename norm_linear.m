2c8dbd66ad099d0318031ebb0c549eda5bb5409b
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 14:13:46 -0200

    Update norm_linear.m

--- norm_linear.m
+++ norm_linear.m
@@ -1,27 +1,26 @@
 function c_norm = norm_linear(c)
 
-%FUNÇÃO QUE REALIZA A NORMALIZAÇÃO LINEAR DAS CARACTERISTICAS DAS CLASSES
+% FUNÇÃO QUE REALIZA A NORMALIZAÇÃO LINEAR DAS CARACTERISTICAS DAS CLASSES
 
-%INPUTS: 
-%		c: célula {1xL}, L é o número de classes
-%		c{1} (N,C), na qual N ´e o numero de padros e N o numero de caracteristicas
+% INPUTS: 
+%	- c: célula {1xL}, L é o número de classes
+%	- c{1} (LxN), L: caracteristicas x N: padroes
 
-%OUTPUTS:	
+% OUTPUTS:	
 %		c_norm: celula com as caracteristicas normalizadas
 
 
 	n_classes = length(c);
-	n_caracteristicas = size(c{1},2);
+	n_caracteristicas = size(c{1},1);
 	vetorzao = [];
 	
 	for i = 1:n_classes
-		vetorzao = [vetorzao;c{i}];
+		vetorzao = [vetorzao;c{i}]; % combinando todas as classes para normalizar os dados
 	end
 	
 	for i = 1:n_classes
-		for j = 1:n_caracteristicas
-			c_norm{i}(:,j) =  ((c{i}(:,j)) - mean(vetorzao(:,j)))./(std(vetorzao(:,j)));
-		end
+		n_padroes = size(c{i},2);
+		c_norm{i} =  c{i} - repmat((mean(vetorzao,2)./(std(vetorzao,2))),1,n_padroes);
 	end
 	
 end
