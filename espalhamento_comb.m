96e3ec59ca6591dc3381218316d7df15da7501c6
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 13:40:58 -0200

    Update espalhamento_comb.m
    Essa função não foi testada ainda

--- espalhamento_comb.m
+++ espalhamento_comb.m
@@ -1,4 +1,4 @@
-function [matriz_Sw,matriz_Sb] = espalhamento(classes)
+function [J1,J,J3,combinacao,matriz_Sw,matriz_Sb,matriz_Sm] = espalhamento(classes,n)
 
 % Autor: André Luiz Costa de Arruda
 % Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
@@ -17,19 +17,26 @@ function [matriz_Sw,matriz_Sb] = espalhamento(classes)
 %OUTPUTS: matrizes de espalhamento	
 %	- matriz_Sw
 %	- matriz_Sb
+%
+
+	n_classes = length(classes); %numero de classes
+	L = size(classes{1},1); %numero de caracteristicas
+	combinacao = combnk(1:L,n); %matriz de combinacao de caracteristicas
+	n_combinacao = size(combinacao,1);% numero de combinacoes
 
     	P=cellfun(@(x)size(x,2),classes); %Verificando o numero de padroes em cada classe
     	P=P./sum(P); %Probabilidade do padrao se encontrar em sua classe
 	dados = [];
 	
-	matriz_media = [];	
-	for i = 1:n_classes
-		matriz_cov = cov(classes{i}',1); % matriz de covariancia normalizada das caracteristicas de cada classe
-		Sw(:,:,i) = P(i) * matriz_cov; % calculando Sw de cada classe para depois fazer o somatorio
-		dados = [dados,classes{i}]; % montando o conjunto de todas as classes
+	for j = 1:n_combinacao
+		for i = 1:n_classes
+			matriz_cov = cov(classes{i}(n_combinacao(j,:),:)',1); % matriz de covariancia normalizada das caracteristicas combinadas de cada classe
+			Sw(:,:,i) = P(i) * matriz_cov; % calculando Sw de cada classe para depois fazer o somatorio
+			dados = [dados,classes{i}]; % montando o conjunto de todas as classes
+		end
+		matriz_Sw{j} = sum(Sw,3); % somatorio das matrizes de covariancia para obter a matriz Sw
+		matriz_Sm{j} = cov(dados(n_combinacao(j,:),:)',1); % matriz_Sm é a covariancia de todas as classes
+		matriz_Sb{j} = matriz_Sm{j} - matriz_Sw{j};
 	end
-	matriz_Sw = sum(Sw,3); % somatorio das matrizes de covariancia para obter a matriz Sw
-        matriz_Sm = cov(dados',1); % matriz_Sm é a covariancia de todas as classes
-        matriz_Sb = matriz_Sm - matriz_Sw;
 
 end
