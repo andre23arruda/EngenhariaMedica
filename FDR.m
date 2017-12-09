900ff17597392d5f45204d2d7c05436fc0ab4e0a
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 20:17:40 -0200

    Update FDR.m

--- FDR.m
+++ FDR.m
@@ -3,15 +3,12 @@ function fdr = FDR(vet1,vet2)
 %FUNCAO PARA CALCULAR O CRITERIO DE SELECAO FDR
 
 % INPUTS:
-%	- vet1: matriz (LxN), L: numero de caracteristicas e C: numero de padroes
-%	- vet2: matriz (LxN), L: numero de caracteristicas e C: numero de padroes
+%	- vet1: vetor de uma caracteristica e com N padroes (1 x N)
+%	- vet2: vetor de outra caracteristica e com N padroes (1 x N)
 
 % OUTPUTs:
-%	fdr: vetor com os valores FDR para cada caracteristica
+%	fdr: FDR calculado para a caracteristica
 	
-	
-	for i = 1:size(vet1,2) % Percorrendo de 1 até o numero de caracteristicas
-		fdr(i) = ((mean(vet1(:,i)) - mean(vet2(:,i)))^2)/(var(vet1(:,i)) + var(vet2(:,i)));
-		% FDR = (u1-u2)^2 / (s1 - s2)^2		da caracteristica de cada classe 
-	end
+	fdr = ((mean(vet1) - mean(vet2))^2)/(var(vet1) + var(vet2));
+	% FDR = (u1-u2)² / (s1² - s2²)	
 end
