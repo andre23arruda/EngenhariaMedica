7c664de907ab14b7ba5f523d02393de841c31607
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 13:52:15 -0200

    Update and rename espalhamento_comb.m to espalhamento_2.m

--- espalhamento_2.m
+++ espalhamento_2.m
@@ -1,4 +1,4 @@
-function [J1,J,J3,combinacao,matriz_Sw,matriz_Sb,matriz_Sm] = espalhamento(classes,n)
+function [J1,J2,J3,combinacao,matriz_Sw,matriz_Sb,matriz_Sm] = espalhamento_2(classes,n)
 
 % Autor: André Luiz Costa de Arruda
 % Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
