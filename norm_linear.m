23e59425f26cd9ddbd8baea03bf79b49c1ae3045
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 14:16:35 -0200

    Update norm_linear.m

--- norm_linear.m
+++ norm_linear.m
@@ -10,17 +10,18 @@ function c_norm = norm_linear(c)
 %		c_norm: celula com as caracteristicas normalizadas
 
 
-	n_classes = length(c);
-	n_caracteristicas = size(c{1},1);
-	vetorzao = [];
+	n_classes = length(c); % numero de classes
+	n_caracteristicas = size(c{1},1); % numero de caracteristicas
+	vetorzao = []; % iniciando um vetor nulo que vai receber os dados de todas as classes combinadas
 	
-	for i = 1:n_classes
+	for i = 1:n_classes % percorrendo de 1 até o numero de classes
 		vetorzao = [vetorzao;c{i}]; % combinando todas as classes para normalizar os dados
 	end
 	
-	for i = 1:n_classes
-		n_padroes = size(c{i},2);
+	for i = 1:n_classes % percorrendo de 1 até o numero de classes
+		n_padroes = size(c{i},2); % verificando o numero de padroes da classes analisada no momento
 		c_norm{i} =  c{i} - repmat((mean(vetorzao,2)./(std(vetorzao,2))),1,n_padroes);
+		% c_normalizado = c - media/desvio
 	end
 	
 end
