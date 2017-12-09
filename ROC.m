5279a2fc61a0f030254790c3b098a992506ee388
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2017-12-09 20:03:40 -0200

    Update ROC.m

--- ROC.m
+++ ROC.m
@@ -15,32 +15,34 @@ function [AUC] = ROC(classe1,classe2,caracteristicas,grafico)
   end
   aux = 0;
 
-  nclasse1 = numel(classe1); %numero de elementos da classe 1
-  nclasse2 = numel(classe2); %numero de elementos da classe 2
+  nclasse1 = numel(classe1); % numero de padroes da classe 1
+  nclasse2 = numel(classe2); % numero de padroes da classe 2
 
-  x = sortrows([classe1, classe2; ones(nclasse1,1), 2*ones(nclasse2,1)]);
-  %criando um vetor com os valores da caracteristica das duas classes e um vetor que corresponde a qual classes os valores pertencem e ordenando os valores
+  x = (sortrows([classe1', ones(nclasse1,1); classe2', 2*ones(nclasse2,1)]))';
+  % criando um vetor com os valores da caracteristica das duas classes e um vetor que corresponde a 
+  % qual classes os valores pertencem e ordenando os valores
 
-  for i = 1:length(x) %fazendo o limiar percorrer o vetor com todos valores do vetor que possui as duas classes
-    VPR(i) = sum(x(2,i:length(x)) == 2)/(nclasse2); %calculando a quantidade de VP a direita do limiar
-    FPR(i) = sum(x(2,i:length(x)) == 1)/(nclasse1); %calculando a quantidade de FP a direita do limiar
+  for i = 1:length(x) % fazendo o limiar percorrer o vetor com todos valores do vetor que possui as duas classes
+    VPR(i) = sum(x(2,i:length(x)) == 2)/(nclasse2); % calculando a quantidade de VP a direita do limiar
+    FPR(i) = sum(x(2,i:length(x)) == 1)/(nclasse1); % calculando a quantidade de FP a direita do limiar
   end 
 
-  VPR(i+1) = 0; %adicionando a posição final como zero
-  FPR(i+1) = 0; %adicionando a posição final como zero
+  VPR(i+1) = 0; % adicionando a posicao final como zero
+  FPR(i+1) = 0; % adicionando a posicao final como zero
 
-  AUC = abs(sum(diff(FPR).*VPR(1:length(VPR)-1))); %calculando a area abaixo da curva atrav�s do somatorio das areas de cada retangulo da curva
+  AUC = abs(sum(diff(FPR).*VPR(1:length(VPR)-1))); % calculando a area abaixo da curva atraves do somatorio das areas de cada retangulo da curva
   
-  if AUC < 0.5 %verificacao se VPR e FPR estao invertidos
-    AUC = 1-AUC; %se sim, AUC = 1-AUC
+  if AUC < 0.5 % verificacao se VPR e FPR estao invertidos
+    AUC = 1-AUC; % se sim, AUC = 1-AUC
     aux = 1;
   end
 
-  if grafico ~= 0
+% ------------------ Aqui é só para plotar -------------------
+  if grafico ~= 0 
     figure('name',['Analise AUC ',caracteristicas]);
     plot((0:1),(0:1),'k','linewidth',2); % criando a linha que divide o grafico ao meio
     hold on;
-    if aux  == 1 %verificacao se VPR e FPR estao invertidos
+    if aux  == 1 % verificacao se VPR e FPR estao invertidos
       plot(VPR,FPR,'linewidth',3);
       xlabel('FPR (\alpha) %');
       ylabel('VPR (1 - \beta) %');
