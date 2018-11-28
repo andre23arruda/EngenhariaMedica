47e5467af76d9164298549f6fa5a3267d112d2d6
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2018-11-28 10:57:50 -0200

    Delete round_precision.m

--- round_precision.m
+++
@@ -1,8 +0,0 @@
-function new = round_precision(n,spaces)
-  entire = fix(n);
-  decimal = n - entire;
-  spaces = 10^spaces;
-  decimal = round(decimal*spaces);
-  decimal = decimal/spaces;
-  new = entire + decimal;
-end
