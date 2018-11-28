4ed2e6a5adb36947d94620e7026c5e120f9793b9
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2018-11-28 10:53:47 -0200

    Update and rename SVD.m to SVDmatrizes.m

--- SVDmatrizes.m
+++ SVDmatrizes.m
@@ -1,11 +1,4 @@
-function [U,S,V] = SVD(matriz)
-
-% Autor: André Luiz Costa de Arruda
-% Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
-% Se quiser, manda uma mensagem no face, whats ou fumaça
-% Pode me seguir no instagram.
-% Sou novo no github, não me julgue.
-% Grato :D
+function [U,S,V] = SVDmatrizes(matriz)
 
     %INPUTS:
     %   - matriz = matriz LxN
@@ -15,7 +8,6 @@ function [U,S,V] = SVD(matriz)
     %   - U = matriz de vetores ortonormais
     %   - S = raiz quadrada dos autovalores
     %   - V = autovetores normalizados
-    %   - novo = matriz com as caracteristicas principais
     
     [L,N] = size(matriz); % dimensoes da matriz de dados (L caracteristicas x N padroes)
     correlacao = (matriz'*matriz);
