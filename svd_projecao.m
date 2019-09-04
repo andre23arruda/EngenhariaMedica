c0ab07533c69d832da66a9da8649560651d049f0
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2019-09-04 15:05:38 -0300

    Update svd_projecao.m

--- svd_projecao.m
+++ svd_projecao.m
@@ -2,11 +2,6 @@ function [U,S,V,novo] = svd_projecao(matriz,m)
 
 
 % Autor: André Luiz Costa de Arruda
-% Se utilizar para algum trabalho, me cite. Eu vou ficar feliz.
-% Se quiser, manda uma mensagem no face, whats ou fumaça
-% Pode me seguir no instagram.
-% Sou novo no github, não me julgue.
-% Grato :D
 
 
     %INPUTS:
